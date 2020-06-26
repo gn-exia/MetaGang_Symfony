286915c2f404d48f2b7584d0cfc8ced814ea2cc5
gn-exia <djamelmiloudi3@gmail.com>
2020-06-26 16:12:50 +0200

    closes #7; HomeController

--- migrations/Version20200626134521.php
+++ migrations/Version20200626134521.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200624124634 extends AbstractMigration
+final class Version20200626134521 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,16 @@ final class Version20200624124634 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE card (id INT AUTO_INCREMENT NOT NULL, clan_id INT NOT NULL, name VARCHAR(255) NOT NULL, skill LONGTEXT NOT NULL, description VARCHAR(255) DEFAULT NULL, url_image VARCHAR(255) NOT NULL, INDEX IDX_161498D3BEAF84C8 (clan_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE clan (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE card ADD CONSTRAINT FK_161498D3BEAF84C8 FOREIGN KEY (clan_id) REFERENCES clan (id)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE card DROP FOREIGN KEY FK_161498D3BEAF84C8');
+        $this->addSql('DROP TABLE card');
         $this->addSql('DROP TABLE clan');
     }
 }
