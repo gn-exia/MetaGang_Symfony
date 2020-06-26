0be093dac59bc33a309c2966f5cb161eccfce945
gn-exia <djamelmiloudi3@gmail.com>
2020-06-26 16:35:09 +0200

    closes #10; Add some faked User in database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,13 +5,30 @@ namespace App\DataFixtures;
 use Faker\Factory;
 use App\Entity\Card;
 use App\Entity\Clan;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    private function encode($user, $plaintextpassword)
+    {
+        return $this->passwordEncoder->encodePassword(
+            $user,
+            $plaintextpassword
+        );
+    }
+
     public function load(ObjectManager $manager)
     {
+
         $faker = Factory::create();
         for ($i = 0; $i < 10; $i++) {
             $oneClan = new Clan();
@@ -26,6 +43,13 @@ class AppFixtures extends Fixture
                 $oneCard->setClan($oneClan);
                 $manager->persist($oneCard);
             }
+        }
+        for ($i = 0; $i < 10; $i++) {
+            $simpleUser = new User();
+            $simpleUser->setUsername(strtolower($faker->firstName()));
+            $simpleUser->setPassword($this->encode($simpleUser, "mdp"));
+            $simpleUser->setRoles(['ROLE_USER']);
+            $manager->persist($simpleUser);
         }
         $manager->flush();
     }
